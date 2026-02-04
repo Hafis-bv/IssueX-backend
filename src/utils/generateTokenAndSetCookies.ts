ed923842488d5c625230f9b6233619f64a8a284a
Abbas Ibragimov <120020090+ibrag1moff@users.noreply.github.com>
2026-02-04 15:03:06 +0400

    Comment out sameSite attribute in cookie settings
    Commented out the sameSite attribute in cookie settings.

--- src/utils/generateTokenAndSetCookies.ts
+++ src/utils/generateTokenAndSetCookies.ts
@@ -16,7 +16,7 @@ export const generateTokenAndSetCookies: GenerateTokenAndSetCookies = (
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    // sameSite: "strict",
     maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 
